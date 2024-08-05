b878bb3ff13327d852c56f3b7dcea04feef80d61
zoskek5429 <zoskek5429@gmail.com>
2024-08-05 17:15:38 +0900

    스택 구현 이해 완료

--- day05_0805월/src/SWEA_8931_제로/Solution.java
+++ day05_0805월/src/SWEA_8931_제로/Solution.java
@@ -12,6 +12,7 @@ public class Solution {
 			int len = sc.nextInt();
 			//전체 숫자가 몇개 들어오는지 = len
 			int[] list = new int[len];
+//			System.out.println("list[top] : "+list[top]);
 			
 			
 			//배열에 순서대로 숫자 넣기
@@ -23,10 +24,14 @@ public class Solution {
 				if(tmp==0) {
 					//이전 값을 뱉어냄
 					//pop 메서드를 호출
+//					System.out.println(i+1+"번 pop전"+Arrays.toString(list));
 					pop(list);
+//					System.out.println(i+1+"번 pop후"+Arrays.toString(list));
 				}
 				else {
+//					System.out.println(i+1+"번 push전"+Arrays.toString(list));
 					push(list, tmp);
+//					System.out.println(i+1+"번 push전"+Arrays.toString(list));
 				}
 			}//이제 남아있는 수의 합을 구하기
 //			int size = stack_list.size();
@@ -34,6 +39,7 @@ public class Solution {
 			for(int i=0; i<list.length;i++) {
 				sum += list[i];
 			}
+//			System.out.println(Arrays.toString(list));
 			System.out.println("#"+t+" "+(int)sum);
 			
 			
@@ -44,6 +50,8 @@ public class Solution {
 	//마지막 값을 뺀 리스트 반환, 마지막 값을 0으로 치환
 	static int[] pop(int[] list) {
 		//마지막 값을 반환
+//		System.out.println("list[top] 뺴기 : "+list[top]);
+		//마지막 인덱스를 한칸 낮게 잡아줌.
 		list[top--]= 0;
 		return list;
 		//마지막 값을 지워줘야함
@@ -52,6 +60,8 @@ public class Solution {
 	//리스트에 값을 입력하는 
 	static int[] push(int[] list, int tmp) {
 		
+		//-1에 1을 더해서 0, 첫 인덱스부터 tmp값을 넣어줌
+		//push를 할때마다 top이 올라감. 리스트의 마지막 입력값의 인덱스를 알려주는 역할.
 		list[++top]=tmp;
 		return list;
 		
